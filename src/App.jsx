091c897201e2dfa1a6b24500fb1515d1af3b9b4d
kevin-dorado <kevindorado2811@gmail.com>
2023-08-16 17:39:57 -0300

    followbutton

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,9 @@ return (
   <div className="my-2 flex">
     <h2 className="text-gray-50  m-2" > A quien seguir</h2>
   </div>
-  <TwitterFolowCard userName="elonmusk" name="@elnaves"/> 
-  <TwitterFolowCard userName="kikobeats" name="@elcubo"/> 
-  <TwitterFolowCard userName="x" name="@x"/> 
+  <TwitterFolowCard userName="elonmusk" name="@elnaves" isFollowing={true}/> 
+  <TwitterFolowCard userName="kikobeats" name="@elcubo"isFollowing={false}/> 
+  <TwitterFolowCard userName="x" name="@x"isFollowing={false}/> 
   </article>
 </>
 
--- src/TwitterFollowCard.jsx
+++ src/TwitterFollowCard.jsx
@@ -1,11 +1,24 @@
-export function TwitterFolowCard({userName,name}){
+export function TwitterFolowCard({userName,name,isFollowing}){
     const imgAvatar = `https://unavatar.io/${userName}`
+    const text = isFollowing ? "Siguiendo"  : "Seguir"
+
+
+    /* 
+        function Button() {
+          var followButton = document.getElementById("buttonfollow")
+
+            if (isFollowing){
+                followButton= <button className="bg-black hover:bg-slate-300 rounded-xl m-2 flex px-3 items-center text-white">Siguiendo</button>  
+                console.log("entro");
+            }else{
+                followButton=<button className="bg-slate-100 hover:bg-slate-300 rounded-xl m-2 flex px-3 items-center text-black">Seguir</button>
+            }
+
+        } */
 
     return(
-
-    <article > 
-      
-            
+    
+    <article >         
         <div class="flex p-1 w-full flex-1 justify-between">
             <div className="flex">
                 <img className="rounded-full w-10 h-10 mt-1 ml-1" 
@@ -17,7 +30,7 @@ export function TwitterFolowCard({userName,name}){
             </div>
             </div>
         
-            <button className="bg-slate-100 hover:bg-slate-300 rounded-xl m-2 flex px-3 items-center text-black">Seguir</button>
+            <button className={`${isFollowing ? "bg-slate-800 hover:bg-slate-500 rounded-xl m-2 flex px-3 items-center text-white" :"bg-slate-100 hover:bg-slate-300 rounded-xl m-2 flex px-3 items-center text-black"}`}>{text}</button>
         </div>
     </article>
     )
