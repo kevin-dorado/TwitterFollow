2a1988d35b857dd73dca89d0965b1213aace7b02
kevin-dorado <kevindorado2811@gmail.com>
2023-08-20 14:51:37 -0300

    finished with location.reload()

--- src/App.jsx
+++ src/App.jsx
@@ -4,47 +4,37 @@ import React from "react"
 import { TwitterFolowCard } from "./TwitterFollowcard"
 import { SearchFollow } from "./SearchFollow"
 
-const Showuser=({users})=>{
-
- return(
-
-  users.map((user)=> (
-    <TwitterFolowCard userName={`${user}`}name={`@${user}`}/> ))
-
- )
-
- 
-
+const Showuser=()=>{
+  const users = JSON.parse(localStorage.getItem("user"))
+  if (users) {
+    return(
+      users.map((user) => {
+        return <TwitterFolowCard userName={user} name={`@${user}`} />;
+      })
+    )
+  }
 }
 
 
+
 export const App = () => {
-
-  const users = localStorage.getItem("user")
-
-
 return (
 
 <div className="flex items-center justify-center min-h-screen  bg-sky-200" >
-<article className="bg-slate-700  m-3 rounded-md  w-96 drop-shadow-lg">
- 
-  <div className="py-2 flex  flex-1 justify-between ">
-    <h2 className="text-gray-50  m-2 font-medium" > A quien seguir</h2>
-    <SearchFollow />
-  </div>
+  <article className="bg-slate-700  m-3 rounded-md  w-96 drop-shadow-lg">
   
-
-  <TwitterFolowCard userName="elonmusk" name="@elnaves" isFollowing={true}/> 
-  <TwitterFolowCard userName="valordolarblue" name="@ValorDolarBlue"isFollowing={false}/> 
+    <div className="py-2 flex  flex-1 justify-between ">
+      <h2 className="text-gray-50  m-2 font-medium" > A quien seguir</h2>
+      <SearchFollow />
+    </div>
+    <div>
+      <TwitterFolowCard userName="elonmusk" name="@elnaves" /> 
+      <TwitterFolowCard userName="valordolarblue" /> 
+      <TwitterFolowCard userName="nicolasamoroso" /> 
+      {Showuser()} 
+    </div>
+ 
   </article>
 </div>
-
-
-
-
 )
-
-
-
-
 }
--- src/SearchFollow.jsx
+++ src/SearchFollow.jsx
@@ -1,6 +1,7 @@
 
 import { Search } from 'lucide-react';
 import { TwitterFolowCard } from './TwitterFollowcard';
+import { App } from './App.jsx';
 
 
 let arrayUser=[]
@@ -10,11 +11,19 @@ const searchUser = ()=>{
 const  user =  document.getElementById("search").value  
 
 if(user!="") {
-
+   const local = JSON.parse(localStorage.getItem("user"))
+   if(local) {
+    arrayUser = local
+   }
     arrayUser.push(user)
     localStorage.setItem("user",JSON.stringify(arrayUser))
+   
 
 
+
+   
+   location.reload(); 
+    
 }
 
 }
@@ -24,18 +33,12 @@ export function SearchFollow(){
     return(
         <>
             <div className='flex justify-center items-center  '>
-            <input id='search' className="  h-7 rounded-l-md   mt-2 m-1 mr-0 bg-slate-500  focus:outline-none  text-gray-50 pl-1"  placeholder="Buscar" type="text"  />
+                <input id='search' className="  h-7 rounded-l-md   mt-2 m-1 mr-0 bg-slate-500  focus:outline-none  text-gray-50 pl-1"  placeholder="Buscar" type="text"  />
 
-            <button onClick={searchUser} className=' h-7 flex justify-center items-center bg bg-slate-600 rounded-r-md mt-1 p-1 mr-3 border-none hover:bg-slate-800'> <Search color='white' size={18}/> </button>
+                <button onClick={searchUser} className=' h-7 flex justify-center items-center bg bg-slate-600 rounded-r-md mt-1 p-1 mr-3 border-none hover:bg-slate-800'> <Search color='white' size={18}/> </button>
 
-
-            
-            </div>
-
-        
-            
+            </div>  
         </>
-       
     
     )   
 }
